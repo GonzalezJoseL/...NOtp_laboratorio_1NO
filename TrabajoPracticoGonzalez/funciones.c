733056d4f94748fe52539b3819f124331d203165
GonzalezJoseL <47353400+GonzalezJoseL@users.noreply.github.com>
2019-04-05 18:07:48 -0300

    Add files via upload

---
+++ TrabajoPracticoGonzalez/funciones.c
@@ -0,0 +1,31 @@
+int suma(int op1, int op2)
+{
+    return op1+op2;
+}
+
+int resta(int op1, int op2)
+{
+    return op1-op2;
+}
+
+int multiplicacion(int op1, int op2)
+{
+    return op1*op2;
+}
+
+float division(int op1, int op2)
+{
+    return (float)op1/op2;
+}
+
+double factorial(int op1)
+{
+   double acumulador;
+
+    for(acumulador=1;op1>1;op1--){
+
+        acumulador*=op1;
+    }
+
+    return acumulador;
+}
---
+++ TrabajoPracticoGonzalez/funciones.h
@@ -0,0 +1,16 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+
+
+#endif // FUNCIONES_H_INCLUDED
+
+int suma(int op1, int op2);
+
+int resta(int op1, int op2);
+
+int multiplicacion(int op1, int op2);
+
+float division(int op1, int op2);
+
+double factorial(int op1);
